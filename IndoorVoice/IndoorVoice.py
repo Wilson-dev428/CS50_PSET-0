3638346fcee1925615b9fd230f70e1162dcc8abe
Wilson-codes34 <kemakolamwilson@gmail.com>
2023-06-18 16:28:57 +0100

    PSET0

---
+++ IndoorVoice/IndoorVoice.py
@@ -0,0 +1,8 @@
+# User input
+
+# Prompts the user to enter in a text
+user_input = input("Enter in a text: ")
+
+
+# Prints the lower case
+print(user_input.lower())
---
+++ IndoorVoice/indoorVoice.md
@@ -0,0 +1,3 @@
+# Indoor Voice
+
+The program is designed to get a user input and turn it all into lower casing
