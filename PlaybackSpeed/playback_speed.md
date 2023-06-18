4c4514ca364c3a7e4c59af6efd020dd19c6fc8aa
Wilson-codes34 <kemakolamwilson@gmail.com>
2023-06-18 16:33:04 +0100

    PSET0

---
+++ PlaybackSpeed/playback_speed.md
@@ -0,0 +1,4 @@
+# Playback Speed
+
+This program prompts the user for input and then outputs that same input, 
+replacing each space with ... (i.e., three periods).
---
+++ PlaybackSpeed/playback_speed.py
@@ -0,0 +1,11 @@
+# Gets the user input
+user_input = input("Enter in a text: ")
+
+# Splits the user answer into a list
+split_answer = user_input.split(" ")
+
+# joins the list together using the ...
+final_answer = "...".join(split_answer)
+
+# Prints the answer
+print(final_answer)
